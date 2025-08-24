e474887434d311f9a22e00049e151ff9f4f1faf7
조윤성 <yunsung1206@gmail.com>
2025-08-24 14:49:27 +0900

    서브넷을 다른 AZ에 순서대로 할당

--- main.tf
+++ main.tf
@@ -11,6 +11,10 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
 
@@ -20,9 +24,10 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "main" {
-  count      = length(var.subnet_cidr_blocks)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_cidr_blocks[count.index]
+  count             = length(var.subnet_cidr_blocks)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr_blocks[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 
   tags = {
     Name = "${var.project_name}-subnet-${count.index + 1}"
