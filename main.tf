101327befac7b52c694bcfff913468d5be7a86c5
조윤성 <yunsung1206@gmail.com>
2025-08-24 16:42:23 +0900

    로드 밸런서 리스너 추가

--- main.tf
+++ main.tf
@@ -139,3 +139,14 @@ resource "aws_lb_target_group" "main" {
     Name = "${var.project_name}-tg"
   }
 }
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
