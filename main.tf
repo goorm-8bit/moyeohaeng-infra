8d74553951fe6b1bea7cf968cac13775ef3ecf0a
조윤성 <yunsung1206@gmail.com>
2025-08-25 22:48:27 +0900

    IAM ECS 실행 역할 생성

--- main.tf
+++ main.tf
@@ -270,6 +270,7 @@ resource "aws_ecs_task_definition" "main" {
   requires_compatibilities = ["EC2"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
@@ -430,4 +431,26 @@ resource "aws_ssm_parameter" "db_password" {
   type      = "SecureString"
   value     = random_password.db_password.result
   overwrite = true
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.project_name}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_execution_role.name
 }
