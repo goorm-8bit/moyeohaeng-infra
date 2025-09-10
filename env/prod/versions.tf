029938cbf88f6b349fb3a94f1f1ef2c88fc0e05f
조윤성 <yunsung1206@gmail.com>
2025-09-10 16:39:45 +0900

    리소스에 공통 태그 적용

--- env/prod/versions.tf
+++ env/prod/versions.tf
@@ -21,4 +21,12 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+    }
+  }
 }
