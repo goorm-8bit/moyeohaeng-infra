5c37c4cc2327bca987aa4242f0a1ba830d539daf
조윤성 <yunsung1206@gmail.com>
2025-09-11 23:39:45 +0900

    Cloud Map 네임스페이스 생성

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -49,3 +49,13 @@ resource "aws_route_table_association" "this" {
   subnet_id      = aws_subnet.this[count.index].id
   route_table_id = aws_route_table.this.id
 }
+
+# 모니터링 및 서비스 간 통신을 위한 Private DNS 네임스페이스 생성
+# VPC 내부에서 서비스들이 이름(DNS)으로 서로 찾을 수 있게 만들어 주는 프라이빗 DNS 영역
+resource "aws_service_discovery_private_dns_namespace" "this" {
+  name = "${var.project_name}.local"
+  vpc  = aws_vpc.this.id
+
+  # 서버 사이드 디스커버리
+  # 클라이언트가 서비스 위치를 알 필요 없이, 서비스 등록과 라우팅을 서버(DNS, LB)가 대신 관리하는 방식
+}
--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -7,3 +7,8 @@ output "subnet_ids" {
   description = "생성된 서브넷 ID의 목록"
   value       = aws_subnet.this[*].id
 }
+
+output "private_dns_namespace_id" {
+  description = "생성된 Private DNS 네임스페이스의 ID"
+  value       = aws_service_discovery_private_dns_namespace.this.id
+}
